5bbc6b839f9ce4c9b74fd1d1f4bf13ec3bd64bb5
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-08-04 02:16:16 +0530

    header made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent,InvestmentResultsComponent,UserInputComponent],
   templateUrl: './app.component.html',
+  styleUrls: []
 })
 export class AppComponent {}
